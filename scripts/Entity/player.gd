2b0a174e0f325c498f3249beaf420ac3c888c434
xquj <a1234zmyh@163.com>
2026-02-10 17:18:01 +0800

    命名规范

--- scripts/Entity/player.gd
+++ scripts/Entity/player.gd
@@ -7,8 +7,6 @@ var item_notes: Array[String]
 var score: int
 var health: int
 var defense: int
-var inventory: Inventory
-var inventory_temporary: Inventory
 var waitingGroup: Array[Card_Base] # 待处理卡牌组（等待进入手牌）。
 var hand_cards: Array[Card_Base] # 手牌列表。
 var showing_cards: Array[Card_Base] # 展示区卡牌列表。
@@ -18,8 +16,6 @@ var can_combo: bool
 func _init(name_: String, team_id_: int, is_active_: bool) -> void:
 	health = 100
 	defense = 100
-	inventory = Inventory.new()
-	inventory_temporary = Inventory.new(999)
 	name = name_
 	team_id = team_id_
 	is_active = is_active_
---
+++ scripts/interactive/end_watting_button.gd
@@ -0,0 +1,37 @@
+extends MeshInstance3D
+
+
+var is_entered: bool
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
+			if is_entered and global.current_state == global.GameState.WAITING:
+				var total_waiting_size: int = 0;
+				for player in global.players:
+					total_waiting_size += player.waitingGroup.size()
+					if !player.can_combo:
+						total_waiting_size = -9999
+				if total_waiting_size == 0 && global.cube_rot_animation.end_value != 90:
+					global.spring_length_animation.set_target(3,200)
+					global.spring_length_animation.play(true)
+					global.spring_rot_x_animation.set_target(0,200)
+					global.spring_rot_x_animation.play(true)
+					global.cube_rot_animation.set_target(90,500)
+					global.cube_rot_animation.play(true)
+
+func _on_area_3d_mouse_entered() -> void:
+	is_entered = true
+
+
+func _on_area_3d_mouse_exited() -> void:
+	is_entered = false
