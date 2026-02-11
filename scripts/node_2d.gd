c600f4b02c965893fe8da1cecceb3a07158ac860
xquj <a1234zmyh@163.com>
2026-02-12 00:07:31 +0800

    小BUG

--- scripts/node_2d.gd
+++ scripts/node_2d.gd
@@ -13,4 +13,4 @@ func _on_button_button_up() -> void:
 	global.players = [LocalPlayer.new("Me", 1, false), MultiPlayer.new("Enemy", 2, false)]
 	global.local_player = global.players[0]
 	global.current_state = global.GameState.DEALING
-	get_tree().change_scene_to_file("res://node_3d.tscn")
+	get_tree().change_scene_to_file("res://scenes/node_3d.tscn")
